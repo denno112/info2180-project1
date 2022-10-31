8648c0e95608d23bcbc3c295723116de16ee2002
denno 2733 <dennismorrison2733@gmail.com>
2022-10-31 09:58:16 -0500

    Updated CSS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+
+
+    let submit = document.querySelector(".newsletter button");
+    let msg = document.querySelector(".message");
+    let email = document.getElementById("email");
+    var reGex = /^\S+@\S+\.\S+$/i;
+
+
+    email.textContent = ""
+
+
+    submit.addEventListener("click",function(a){
+        a.preventDefault();
+        let confirm = email.Value;
+        if (confirm === "" || !reGex.test(confirm)){
+            msg.textContent = "Please enter a valid email address"
+        }else{
+            msg.textContent = "Thank You! Your email address " + confirm.toString() + " has been added to our mailing list!"
+        }
+    });
+};
