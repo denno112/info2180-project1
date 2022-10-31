ba82efba3ff2c330628b67033ca256ec9e97a8dd
denno 2733 <dennismorrison2733@gmail.com>
2022-10-31 10:19:10 -0500

    Updated Js

--- scripts.js
+++ scripts.js
@@ -2,22 +2,23 @@
 window.onload = function(){
 
 
-    let submit = document.querySelector(".newsletter button");
+    let container = document.getElementsByClassName("newsletter")[0];
+    let sub = container.getElementsByTagName("button")
     let msg = document.querySelector(".message");
     let email = document.getElementById("email");
     var reGex = /^\S+@\S+\.\S+$/i;
 
 
     email.textContent = ""
+    sub[0].addEventListener('click', function(a){
+        a.preventDefault()
+        let confirmemail = email.value
 
-
-    submit.addEventListener("click",function(a){
-        a.preventDefault();
-        let confirm = email.Value;
-        if (confirm === "" || !reGex.test(confirm)){
-            msg.textContent = "Please enter a valid email address"
+        if (confirmemail === "" || !reGex.test(confirmemail)) {
+            msg.innerHTML  = "Please enter a valid email address"
         }else{
-            msg.textContent = "Thank You! Your email address " + confirm.toString() + " has been added to our mailing list!"
+            msg.innerHTML = `Thank You! Your email address ${confirmemail} has been added to our mailing list!`
         }
-    });
+    })
+
 };
